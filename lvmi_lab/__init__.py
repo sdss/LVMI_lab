b87e5486272d73bfadae7afee18d920e12ba778c
Nick Konidaris <nick.konidaris@gmail.com>
2023-08-16 13:49:18 -0700

    Update __init__.py

--- lvmi_lab/__init__.py
+++ lvmi_lab/__init__.py
@@ -4,6 +4,7 @@ from .lvmi_lab import generate_fake_frame
 from .lvmi_lab import generate_shifted_frame
 from .lvmi_lab import xcor_frames
 from .lvmi_lab import really_fake_line_frame
+from .lvmi_lab import hartman_focus_by_peak_finding
 from .lvmi_lab import get_positions_on_ccd
 from .lvmi_lab import shift_frame
 from .bias_subtract import subtract_overscan
